2c4500e946c952ee6ef298f6312cca131a4c1b03
Arijit Saha <32124723+arijit-saha@users.noreply.github.com>
2017-10-24 20:51:36 +0530

    Update bubble-sort.c

--- bubble-sort.c
+++ bubble-sort.c
@@ -2,36 +2,36 @@
 #include<conio.h>
 #define MAX 20
 void bubble_sort(int arr[],int size) {
-	int i,j,temp;
-	for (i = 0; i < size-1; ++i) {
-		for (j = 0; j < size-1-i; ++j) {
-			if (arr[j]>arr[j+1]) {
-				temp = arr[j];
-				arr[j] = arr[j+1];
-				arr[j+1] = temp;
-			}
+int i,j,temp;
+for (i = 0; i < size-1; ++i) {
+	for (j = 0; j < size-1-i; ++j) {
+		if (arr[j]>arr[j+1]) {
+			temp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = temp;
 		}
 	}
 }
+}
 
 void main() {
-	int arr[MAX], i, j, n;
-	clrscr();
-	printf("Enter the number of elements : \n");
-	scanf("%d",&n);
-	for (i = 0; i < n; ++i) {
-		printf("Enter elements %d : ",i+1);
-		scanf("%d",&arr[i]);
-	}
-	printf("Unsorted list is : \n");
-	for (i = 0; i < n; ++i)
-		printf("%d",arr[i]);
-	printf("\n");
+int arr[MAX], i, j, n;
+clrscr();
+printf("Enter the number of elements : \n");
+scanf("%d",&n);
+for (i = 0; i < n; ++i) {
+	printf("Enter elements %d : ",i+1);
+	scanf("%d",&arr[i]);
+}
+printf("Unsorted list is : \n");
+for (i = 0; i < n; ++i)
+	printf("%d",arr[i]);
+printf("\n");
 
-	bubble_sort(arr,n);
+bubble_sort(arr,n);
 
-	printf("Sorted list is  : \n");
-	for (i = 0; i < n; ++i)
-		printf("%d",arr[i]);
-	printf("\n");
+printf("Sorted list is  : \n");
+for (i = 0; i < n; ++i)
+	printf("%d",arr[i]);
+printf("\n");
 }
